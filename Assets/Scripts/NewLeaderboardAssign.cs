d55b6c146115758f64ed889fe043b1b0a374211f
SheiinX <sebastianfrancocatano@gmail.com>
2024-04-23 09:26:30 -0500

    leaderboard base working

--- Assets/Scripts/NewLeaderboardAssign.cs
+++ Assets/Scripts/NewLeaderboardAssign.cs
@@ -15,7 +15,38 @@ public class NewLeaderboardAssign : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetUsersHighestScores();
+        FirebaseDatabase.DefaultInstance.GetReference("users").OrderByChild("score").LimitToLast(3).ValueChanged += HandleValueChanged;
+        //GetUsersHighestScores();
+    }
+
+    private void HandleValueChanged(object sender, ValueChangedEventArgs args)
+    {
+        if(args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+
+        DataSnapshot snapshot = args.Snapshot;
+
+        var _leaderboard = gameObject.GetComponentsInChildren<ScoreEntry>();
+        foreach (var item in _leaderboard)
+        {
+            Destroy(item.gameObject);
+        }
+
+        int i = 0;
+        foreach (var userDoc in (Dictionary<string, object>)snapshot.Value)
+        {
+            var userObject = (Dictionary<string, object>)userDoc.Value;
+            Debug.Log($"{userObject["username"]} : {userObject["score"]}");
+
+            var scoreEntryGO = GameObject.Instantiate(scoreEntryPrefab, transform);
+            scoreEntryGO.transform.position = new Vector2(scoreEntryGO.transform.position.x, transform.position.y - i * _spacedBoard);
+            scoreEntryGO.GetComponent<ScoreEntry>().SetLabels($"{userObject["username"]}", $"{userObject["score"]}");
+
+            i++;
+        }
     }
 
     private void GetUsersHighestScores()
